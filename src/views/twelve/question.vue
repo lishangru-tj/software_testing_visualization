f4cfa0ac08730c46a18c8750dc56e1953a34bec5
lishangru <947112895@qq.com>
2023-06-21 02:01:02 +0800

    合并冲突解决
    解决了一些合并时的冲突

--- src/views/twelve/question.vue
+++ src/views/twelve/question.vue
@@ -1,22 +1,4 @@
 <template>
-<<<<<<< Updated upstream
-  <div>
-    <span style="font-size:24px;"><b>&nbsp;第十二题</b></span>
-    <div style="font-size:16px;">
-
-      <p>按照下列C语言程序前的编号，构建起程序图（control flow graph or program diagram），不需要理解具体的代码实现内容，只要理解其逻辑即可。</p>
-      <img src="./12.jpg" />
-
-      <p style="margin-top:100px;font-size:20px;">程序图如下：</p>
-    </div>
-
-    <img src="./answer.jpg" width="100%" />
-
-
-
-
-  </div>
-=======
     <div>
         <span style="font-size:24px;"><b>&nbsp;第十二题＆十五题</b></span>
       <div style="font-size:16px;">
@@ -65,7 +47,6 @@
       
     </div>
 
->>>>>>> Stashed changes
 </template>
 
 <script>
@@ -87,9 +68,6 @@ export default {
 };
 </script>
 
-<<<<<<< Updated upstream
-<style scoped lang="less"></style>
-=======
 <style scoped lang="less">
     .tree {
       font-family: Arial, sans-serif;
@@ -125,4 +103,3 @@ export default {
     }
 
 </style>
->>>>>>> Stashed changes
