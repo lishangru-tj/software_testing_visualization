4eed7cee9f3f44ddeadd870dc9c2a297315c9697
sbw2053512 <83405314+sbw2053512@users.noreply.github.com>
2023-06-22 01:48:35 +0800

    17

--- src/views/seventeen/systemtest.vue
+++ src/views/seventeen/systemtest.vue
@@ -4,14 +4,18 @@
       <el-select v-model="value" placeholder="请选择测试方法">
         <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
       </el-select>
-      <!-- <div class="button-group">
+      <div class="button-group">
         <el-button class="main-button" type="success" plain @click="doTest" :loading="loading">进行测试<i
             class="el-icon-upload el-icon--right"></i></el-button>
         <el-button @click="reset(value)" class="reset-button" type="warning" plain>重置</el-button>
-      </div> -->
+      </div>
     </div>
     <el-divider content-position="left">测试用例</el-divider>
 
+    <div>
+      <h6 >{{ introduce }}</h6>
+    
+    </div>
     <div class="main-table">
       <el-table :data="tableData" :height="tableHeight" border style="width: 100%;height: 100%;" v-loading="loading"
         :row-class-name="tableRowClassName">
@@ -21,24 +25,14 @@
         <el-table-column prop="请假天数" label="请假天数" width="50" align="center"></el-table-column>
         <el-table-column prop="佣金系数" label="佣金系数" align="center"></el-table-column>
         <el-table-column prop="real_money" label="实际佣金(万元)" align="center">
-          <!-- <template slot-scope="scope">
-            <div v-if="scope.row.state == true" class="icon-svg">
-              <i class="el-icon-check"></i><span>测试通过</span>
-            </div>
-            <div v-if="scope.row.state == false" class="icon-svg">
-              <i class="el-icon-close"></i><span>测试未通过</span>
-            </div>
-          </template> -->
         </el-table-column>
         <el-table-column prop="语句覆盖" label="语句覆盖" width="60" align="center"></el-table-column>
         <el-table-column prop="判断覆盖" label="判断覆盖" width="60" align="center"></el-table-column>
         <el-table-column prop="条件覆盖" label="条件覆盖" width="60" align="center"></el-table-column>
-        <el-table-column prop="1" label="条件1:销售额>2.0" align="center"></el-table-column>
-        <el-table-column prop="2" label="条件2:请假天数<=10" align="center"></el-table-column>
-        <el-table-column prop="3" label="条件3:现金到帐>=60.0" align="center"></el-table-column>
-        <el-table-column prop="条件组合覆盖" label="条件组合覆盖" align="center"></el-table-column>
+        <el-table-column prop="覆盖组合号" label="覆盖组合号" align="center"></el-table-column>
       </el-table>
     </div>
+    <img id="5" src="./132.png" style="display: none;">
     <img src="./13.png"/>
   </div>
 </template>
@@ -67,7 +61,7 @@ export default {
       loading: false,
       classState: [],
       json: {},
-      introduce: "边界值法",
+      introduce: "无条件语句的序列作为一个节点，对所有节点进行覆盖",
     };
   },
   computed: {
@@ -119,6 +113,7 @@ export default {
       this.loading = true;
       _this.tableData.forEach((item, index) => {
         console.log(item["real_money"])
+        
         if (item["佣金系数"] != "N/A")
           item["real_money"] = Math.floor(item["销售额（百万元）"] / item["佣金系数"] * 100) / 100;
         else item["real_money"] = 0
@@ -128,25 +123,30 @@ export default {
     reset(value) {
       if (value === "1") {
         this.json = mock_1_json;
-        this.introduce = "语句覆盖"
+        this.introduce = "语句覆盖:无条件语句的序列作为一个节点，对所有节点进行覆盖"
+        document.getElementById("5").style.display="none"
         this.initTableData(mock_1_json);
       }
       else if (value === "2") {
-        this.introduce = "判断覆盖"
+        this.introduce = "判断覆盖:每个判断的真分支和假分支都至少经历一次"
+        document.getElementById("5").style.display="none"
         this.json = mock_2_json;
         this.initTableData(mock_2_json);
       }
       else if (value === "3") {
         this.json = mock_3_json;
-        this.introduce = "条件覆盖"
+        document.getElementById("5").style.display="none"
+        this.introduce = "条件覆盖:要使得每个判断中的每个条件的可能取值至少满足一次"
         this.initTableData(mock_3_json);
       } else if (value === "4") {
         this.json = mock_4_json;
-        this.introduce = "判断——条件覆盖"
+        document.getElementById("5").style.display="none"
+        this.introduce = "判断——条件覆盖:一个判定中的每个条件的所有可能结果至少执行一次，并且每个判断本身的所有可 能结果至少执行一次。"
         this.initTableData(mock_4_json);
       } else if (value === "5") {
         this.json = mock_5_json;
-        this.introduce = "条件组合覆盖"
+        document.getElementById("5").style.display=""
+        this.introduce = "条件组合覆盖:一个判定中的每个条件的所有可能结果至少执行一次，并且每个判断本身的所有可 能结果至少执行一次。"
         this.initTableData(mock_5_json);
       }
     }
